82d440785e29c557dafd29dee94b309fec229386
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-08-06 15:44:02 +0530

    Ready to deploy4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
-end
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from StandardError, with: :render_internal_server_error
+  
+    private
+  
+    def record_not_found
+      render file: 'public/404.html', status: :not_found
+    end
+  
+    def render_internal_server_error(exception)
+      logger.error(exception.message)
+      render file: 'public/500.html', status: :internal_server_error
+    end
+  end
+  
